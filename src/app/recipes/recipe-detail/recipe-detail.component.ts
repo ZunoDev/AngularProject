7a935e597630fafa764584be1715ec28ca17f871
ZunoDev <gilsonsilva.jesus@gmail.com>
2018-09-16 01:01:12 -0300

    Navigation Improvements

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Recipe } from '../recipe.model';
+import { Component, OnInit, Input } from "@angular/core";
+import { Recipe } from "../recipe.model";
+import { ActivatedRoute, Params } from "@angular/router";
+import { RecipeService } from "../recipe.service";
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.css"]
 })
 export class RecipeDetailComponent implements OnInit {
+  @Input()
+  recipe: Recipe;
 
-  @Input() recipe: Recipe;
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.recipe = this.recipeService.getRecipe(+params["id"]);
+    });
   }
-
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,20 +1,34 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { Recipe } from './recipe.model';
+import { Injectable, EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class RecipeService {
-
-  recipeSelected = new  EventEmitter<Recipe>();
-  private recipes: Recipe[] = [{name: 'Recipe One', description: 'A Special recipe',
-                      ImagePath: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'},
-                      {name: 'Recipe Two', description: 'A Special recipe',
-                      ImagePath: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'}
-                    ];
-  constructor() { }
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    {
+      id: 1,
+      name: "Recipe One",
+      description: "A Special recipe",
+      ImagePath:
+        "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+    },
+    {
+      id: 2,
+      name: "Recipe Two",
+      description: "A Special recipe",
+      ImagePath:
+        "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+    }
+  ];
+  constructor() {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(id) {
+    return this.recipes.find(r => r.id === id);
   }
 }
