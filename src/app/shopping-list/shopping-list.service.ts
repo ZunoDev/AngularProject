7ccdfe3a1d70f99fa1874f5157acc80a29e8b9ed
ZunoDev <gilsonsilva.jesus@gmail.com>
2018-10-03 17:01:13 -0300

    Subject changes

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient-model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+  ingredientsChanged = new Subject<Ingredient[]>();
 
-  private ingredients: Ingredient[] = [{Name: 'First Ingredient', Amount: 5 }];
-
-  constructor() { }
+  private ingredients: Ingredient[] = [
+    { Name: 'Apples', Amount: 5 },
+    { Name: 'Tomatoes', Amount: 10 }
+  ];
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  addIngredients(ingredient: Ingredient[]) {
+    this.ingredients.push(...ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
